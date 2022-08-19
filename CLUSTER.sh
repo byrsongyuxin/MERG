6c8180ca90d8dc92ecc672657ef55505bf6d53c0
anahuauda <42733639+byrsongyuxin@users.noreply.github.com>
2022-08-19 16:56:58 +0800

    Update CLUSTER.sh

--- CLUSTER.sh
+++ CLUSTER.sh
@@ -1,9 +1,9 @@
 python main_SBMs_node_classification_best_model.py --dataset SBM_CLUSTER \
 --gpu_id 0 --config 'configs/SBMs_node_clustering_GAT_CLUSTER_500k.json' \
---batch_size 32 --out_dir ./output/CLUSTER_GAT/ \
+--batch_size 16 --out_dir ./output/CLUSTER_MERG_GAT/ \
 --dropout 0.1 --max_time 60 \
 
 python main_SBMs_node_classification_best_model.py --dataset SBM_CLUSTER \
 --gpu_id 0 --config 'configs/SBMs_node_clustering_GatedGCN_PATTERN_PE_500k.json' \
---batch_size 32 --out_dir ./output/CLUSTER_GatedGCN/ \
+--batch_size 16 --out_dir ./output/CLUSTER_MERG_GatedGCN/ \
 --dropout 0.1 --max_time 60 \
