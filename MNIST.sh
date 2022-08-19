a7778ed0d0b9168d7068c057bb142a3f926d2d2e
anahuauda <42733639+byrsongyuxin@users.noreply.github.com>
2022-08-19 16:49:58 +0800

    Update MNIST.sh

--- MNIST.sh
+++ MNIST.sh
@@ -1,9 +1,9 @@
 python main_superpixels_graph_classification_best_model.py --dataset MNIST \
 --gpu_id 0 --config 'configs/superpixels_graph_classification_GAT_MNIST_100k.json' \
---batch_size 128 --out_dir ./output/MNIST_GAT/ \
+--batch_size 128 --out_dir ./output/MNIST_MERG_GAT/ \
 --dropout 0.1 --max_time 60 \
 
 python main_superpixels_graph_classification_best_model.py --dataset MNIST \
 --gpu_id 0 --config 'configs/superpixels_graph_classification_GatedGCN_MNIST_100k.json' \
---batch_size 64 --out_dir ./output/MNIST_GAT/ \
+--batch_size 64 --out_dir ./output/MNIST_MERG_Gated-GCN/ \
 --dropout 0.1 --max_time 60 \
